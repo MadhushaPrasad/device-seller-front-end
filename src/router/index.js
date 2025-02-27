d0e09cbda820b96d6db9e4d1cc89a0c4dd1375b4
Madhusha Prasad <madushaprasad21@gmail.com>
2025-02-27 09:39:00 +0530

    fix: update eslint-plugin-vue to version 8.7.1 and adjust routing names

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Login from "../views/Login.vue";
 const routes = [
   {
     path: "/",
-    name: "home",
+    name: "login",
     component: Login,
   },
   {
@@ -14,7 +14,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "about" */ "../views/Admin.vue"),
   },
 ];
 
