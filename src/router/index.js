3afcdc89ad6a2bf3427f8513e49cc1291e646f40
Madhusha Prasad <madushaprasad21@gmail.com>
2025-02-27 16:25:36 +0530

    chore: add new routes for Home, Register, Profile, and Admin with 404 and 401 error handling

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,50 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login.vue";
+import Home from "@/views/Home.vue";
+import Register from "@/views/Register.vue";
+import Profile from "@/views/Profile.vue";
+import Admin from "@/views/Admin.vue";
 
 const routes = [
   {
     path: "/",
-    name: "login",
+    component: Home,
+  },
+  {
+    path: "home",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "login",
+    name: "Login",
     component: Login,
   },
   {
-    path: "/register",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Admin.vue"),
+    path: "register",
+    name: "Register",
+    component: Register,
   },
+  {
+    path: "profile",
+    name: "Profile",
+    component: Profile,
+  },
+  {
+    path: "admin",
+    name: "Admin",
+    component: Admin,
+  },
+  {
+    path: "404",
+    name: "/404",
+    component: Login,
+  },
+  {
+    path: "401",
+    name: "/401",
+    component: Login,
+  }
 ];
 
 const router = createRouter({
