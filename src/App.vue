ea273f10e3ded3ac506bb122af3fa02b72b6ab92
Madhusha Prasad <madushaprasad21@gmail.com>
2025-02-28 12:49:36 +0530

    chore: refactor App.vue for improved navigation layout and styling

--- src/App.vue
+++ src/App.vue
@@ -3,98 +3,28 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div>
-    <nav class="bg-dark navbar navbar-expand navbar-dark">
-      <a href="#" class="ms-1 navbar-brand">
+  <div class="w-full h-full">
+    <nav class="flex items-center bg-gray-700 px-4 py-3 w-full text-white">
+      <a href="#" class="flex items-center space-x-2">
         <img src="./assets/logo.svg" width="40" alt="Vue logo" />
-
-        Vue
+        <span class="font-semibold text-lg">Vue</span>
       </a>
 
-      <div class="me-auto navbar-nav">
-        <li class="nav-item">
-          <router-link to="/admin" class="nav-link"> Admin </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/home" class="nav-link"> Home </router-link>
-        </li>
+      <div class="flex space-x-4 ml-6">
+        <RouterLink to="/admin" class="hover:text-gray-300">Admin</RouterLink>
+        <RouterLink to="/home" class="hover:text-gray-300">Home</RouterLink>
       </div>
 
-      <div class="ms-auto navbar-nav">
-        <li class="nav-item">
-          <router-link to="/register" class="nav-link"> Signup </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/login" class="nav-link"> Login </router-link>
-        </li>
+      <div class="flex space-x-4 ml-auto">
+        <RouterLink to="/register" class="hover:text-gray-300">Signup</RouterLink>
+        <RouterLink to="/login" class="hover:text-gray-300">Login</RouterLink>
       </div>
     </nav>
+
     <div class="p-4 container">
       <RouterView />
     </div>
   </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
