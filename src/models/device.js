4f3995241808a9a509271f5b1847cad8a4cfb083
Madhusha Prasad <madushaprasad21@gmail.com>
2025-02-28 15:26:45 +0530

    feat: add Device model for device management

---
+++ src/models/device.js
@@ -0,0 +1,10 @@
+export default class Device {
+  constructor(id, name,description, price, createTime, deviceType) {
+    this.id = id
+    this.name = name
+    this.description = description
+    this.price = price
+    this.createTime = createTime
+    this.deviceType = deviceType
+  }
+}
