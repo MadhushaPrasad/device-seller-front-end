9183ac5115bc392e86ef41ed884412df96dc93b7
Madhusha Prasad <madushaprasad21@gmail.com>
2025-02-28 15:28:03 +0530

    feat: add Purchase model for managing purchase data

---
+++ src/models/purchase.js
@@ -0,0 +1,10 @@
+export default class Purchase {
+  constructor(id, userId, deviceId,price, color, purchaseTime) {
+    this.id = id
+    this.userId = userId
+    this.deviceId = deviceId
+    this.price = price
+    this.color = color
+    this.purchaseTime = purchaseTime
+  }
+}
