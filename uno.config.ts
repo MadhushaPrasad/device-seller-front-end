90d788b146d614fc5d4f7e689e761b7b8e82576b
Madhusha Prasad <madushaprasad21@gmail.com>
2025-02-28 11:55:10 +0530

    chore: update UnoCSS configuration and remove Tailwind CSS reset import

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,9 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, presetAttributify, presetUno } from 'unocss'
 
 export default defineConfig({
-  // ...UnoCSS options
+  presets: [
+    presetAttributify(),
+    presetUno(),
+  ],
+  rules: [],
 })
