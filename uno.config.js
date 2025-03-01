febf1eb2805e78bdd48386cb59dfde22aadab984
Madhusha Prasad <madushaprasad21@gmail.com>
2025-03-01 09:09:35 +0530

    feat: add Iconify and Unocss preset icons for enhanced icon management

--- uno.config.js
+++ uno.config.js
@@ -1,7 +1,15 @@
+import UnocssIcons from '@unocss/preset-icons'
 import { defineConfig, presetAttributify, presetWind } from 'unocss'
 
 export default defineConfig({
   presets: [
+    UnocssIcons({
+      // options
+      prefix: 'i-',
+      extraProperties: {
+        display: 'inline-block'
+      }
+    }),
     presetAttributify(),
     presetWind(),
   ],
