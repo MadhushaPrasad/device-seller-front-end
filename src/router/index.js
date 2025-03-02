78c10249769e433df4950bbddd0cc10a7f1c3148
Madhusha Prasad <madushaprasad21@gmail.com>
2025-03-02 22:17:02 +0530

    feat: implement role-based access control in router with navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -6,58 +6,86 @@ import Profile from '@/views/Profile.vue'
 import Admin from '@/views/Admin.vue'
 import UnauthorizedAccess from '@/views/UnauthorizedAccess.vue'
 import NotFound from '@/views/NotFound.vue'
+import Role from '@/models/role'
+import Store from '@/stores/index'
+const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile,
+    meta: {
+      roles: [Role.USER, Role.ADMIN],
+    },
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin,
+    meta: {
+      roles: [Role.ADMIN],
+    },
+  },
+  {
+    path: '/not-found',
+    name: '/404',
+    component: NotFound,
+  },
+  {
+    path: '/unauthorized',
+    name: '/401',
+    component: UnauthorizedAccess,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('@/views/Admin.vue'),
+  },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      component: Home,
-    },
-    {
-      path: '/home',
-      name: 'Home',
-      component: Home,
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login,
-    },
-    {
-      path: '/register',
-      name: 'Register',
-      component: Register,
-    },
-    {
-      path: '/profile',
-      name: 'Profile',
-      component: Profile,
-    },
-    {
-      path: '/admin',
-      name: 'Admin',
-      component: Admin,
-    },
-    {
-      path: '/not-found',
-      name: '/404',
-      component: NotFound,
-    },
-    {
-      path: '/unauthorized',
-      name: '/401',
-      component: UnauthorizedAccess,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/views/Admin.vue'),
-    },
-  ],
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const { roles } = to.meta
+  const currentUser = Store.getters['currentUser']
+
+  if (!roles?.length) {
+    if (currentUser) {
+      return next({ path: '/login' })
+    } else {
+      return next()
+    }
+  }
+
+  if (!roles?.includes(currentUser.role)) {
+    return next({ path: '/unauthorized' })
+  }
+
+  next()
 })
 
 export default router
