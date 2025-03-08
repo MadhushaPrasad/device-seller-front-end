0eed3ebe3c19b784925d55693ba303c3bda239e2
Madhusha Prasad <madushaprasad21@gmail.com>
2025-03-08 23:51:32 +0530

    refactor: convert DeviceService methods to use concise syntax

--- src/services/device.service.js
+++ src/services/device.service.js
@@ -7,18 +7,18 @@ import { authHeader } from './base.service'
 const API_URL = `${BASE_API_URL}devices`
 
 class DeviceService {
-  saveDevice = async (device) => {
-    const response = await axios.post(API_URL, device, { headers: authHeader() })
+  saveDevice(device) {
+    const response = axios.post(API_URL, device, { headers: authHeader() })
     return response
   }
 
-  deleteDevice = async (device) => {
-    const response = await axios.delete(`${API_URL}/${device.id}`, { headers: authHeader() })
+  deleteDevice(device) {
+    const response = axios.delete(`${API_URL}/${device.id}`, { headers: authHeader() })
     return response
   }
 
-  getAllDevices = async () => {
-    const response = await axios.get(API_URL)
+  getAllDevices() {
+    const response = axios.get(API_URL)
     return response
   }
 }
